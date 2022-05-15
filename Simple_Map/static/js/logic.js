ec16b650188c4d40178984c157e1657a656e18fe
rmward17 <wardrachelm17@gmail.com>
2022-05-15 11:03:24 -0400

    Mapping a single point

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,6 +4,8 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
+
+
 // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
 //     center: [
